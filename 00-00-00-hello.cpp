a09e980fcc7cf9293c0caa1f76bdab4a9734f225
张会亮 <huiliang.zhang@17zuoye.com>
2021-03-22 21:40:29 +0800

    hello world

---
+++ 00-00-00-hello.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "-----------------------------------------" << endl << endl;
+    cout << "Hello world" << endl;
+    return 0;
+}
+
+
+
