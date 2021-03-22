ad26e3d7f7007ab4513b0a02bf5aeaebe35f9329
张会亮 <huiliang.zhang@17zuoye.com>
2021-03-22 22:09:36 +0800

    data type

---
+++ 02-01-01-data-type.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int i = 1;
+    cout << i << endl;
+
+    short i2 = 2;
+    cout << i2 << endl;
+
+    long i3 = 3;
+    cout << i3 << endl;
+
+    long long i4 = 4;
+    cout << i4 << endl;
+
+    unsigned i5 = 5;
+    cout << i5 << endl;
+
+    unsigned long i6;
+    cout << "Please input a number:";
+    cin >> i6;
+    cout << i6 << endl;
+
+    const unsigned long long i7(7);
+    cout << i7 << endl;
+
+    // i7 = 8;
+
+    const double pi(3.14159);
+    cout << pi << endl;
+
+
+    char a = 'a';
+    cout << a << endl;
+
+
+    const char b[] = "abc";
+    cout << &b << endl;
+
+
+    return 0;
+}
+
+
+
+
