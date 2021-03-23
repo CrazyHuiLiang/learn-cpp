8416210c4776fb236fad4bc681ec032bc30016dc
HuiLiang <CrazyHuiLiang@163.com>
2021-03-23 09:26:02 +0800

    function call

---
+++ 03-01-026call.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+// srand 伪随机数种子（随机数的起始值）
+// rand 伪随机数
+
+
+int main(int argc, char *argv[]) {
+	for(int i = 0; i< 100; i++) {
+		cout << rand()%6 << endl;
+	}
+	return 0;
+}
+
+
